4d2ca0075bdaba9a62cc2b979662f930226f3ecf
Sujatha yalamarthi <sujatha.yalamarthi24@sasi.ac.in>
2026-07-14 11:34:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const dns=require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://sujathayalamarthi-24:<db_password>@cluster0.n7izdpt.mongodb.net/'); 
+    await mongoose.connect('mongodb+srv://sujathayalamarthi-24:suji321@cluster0.n7izdpt.mongodb.net/'); 
     console.log('MongoDB Connected to studentDB');
   } catch (err) {
     console.error(err);
